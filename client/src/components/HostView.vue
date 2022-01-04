4ca69fc125d72e2eeecba686a51b13239240a91b
Steph Ridnell <steph.ridnell@gmail.com>
2022-01-04 17:29:30 +1100

    Added title

--- client/src/components/HostView.vue
+++ client/src/components/HostView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="host-view">
+    <div class="text-center white p-20 text-bold text-caps text-60">
+      Steph<span class="strikethrough">Mar</span>io Party
+    </div>
     <div class="container p-30">
       <div class="box" v-for="(team, index) in maxTeams" :key="index" style="border-radius:64px;box-shadow: rgb(0 0 0 / 35%) 0px 0px 18px 0px;">
-        <div class="card mb-26" :style="'background:var(--player' + index + ');box-shadow: 0 12px 0px 0px var(--player' + index + 'darker)'">
+        <div class="card mb-26" :class="{ 'greyscale': currentTeams[index] && !connectedUsers.includes(currentTeams[index].userId) }" :style="'background:var(--player' + index + ');box-shadow: 0 12px 0px 0px var(--player' + index + 'darker)'">
           <div class="p-20 card-body" v-if="currentTeams[index]">
             <div class="d-flex ai-center">
               <img class="avatar mr-12" :src="currentTeams[index].avatar" :style="'background:var(--player' + index + 'darker);'" />
@@ -16,7 +19,7 @@
               </div>
             </div>
 
-            <div class="text-32 text-bold text-caps" v-if="!connectedUsers.includes(currentTeams[index].userId)">
+            <div class="text-bold text-caps" v-if="!connectedUsers.includes(currentTeams[index].userId)">
               Offline
             </div>
           </div>
@@ -33,7 +36,7 @@
       </div>
     </div>
     <div class="text-center">
-      <button>
+      <button @click="startGame" :disabled="!ready">
         Start
       </button>
     </div>
@@ -50,11 +53,19 @@ export default {
       gameId: 'gameId',
       connectedUsers: 'connectedUsers',
       currentTeams: 'currentTeams'
-    })
+    }),
+    ready () {
+      return this.maxTeams === this.currentTeams.length
+    }
   },
   data () {
     return {
       maxTeams: 4
+    }
+  },
+  methods: {
+    startGame () {
+      console.log('start')
     }
   }
 }
