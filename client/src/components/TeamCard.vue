e3a154b9f4ec317e8862139c90f439f002fa1798
Steph Ridnell <steph.ridnell@gmail.com>
2022-01-05 12:09:35 +1100

    Added minimised view

--- client/src/components/TeamCard.vue
+++ client/src/components/TeamCard.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="box">
     <div class="card mb-26" :class="{ 'greyscale': team && !connectedUsers.includes(team.userId) }" :style="'background:linear-gradient(180deg, var(--player' + index + ') 0%, var(--player' + index + 'darker) 100%);box-shadow: 0 12px 0px 0px var(--player' + index + 'darkest)'">
-      <div class="p-20 card-body" v-if="team">
-        <div class="d-flex ai-center">
+      <div class="card-body" :class="{ 'p-20': !minimised }" v-if="team">
+        <div class="d-flex ai-center" v-if="!minimised">
           <img class="avatar" :class="{ 'mr-12': team.players && team.players[0] }" :src="team.avatar" :style="'background:var(--player' + index + 'darkest);'" />
           <img v-if="team.players && team.players[0]" class="avatar" :src="team.players[0].avatar" :style="'background:var(--player' + index + 'darkest);'" />
         </div>
 
-        <div class="d-flex ai-center text-24 text-bold text-caps mt-12">
+        <div class="d-flex ai-center text-24 text-bold text-caps mt-12" v-if="!minimised">
           {{ team.teamCaptain || '?' }}
           <div v-if="team.players && team.players[0]">
             &nbsp;&amp; {{ team.players[0].name }}
           </div>
         </div>
 
-        <div v-if="gameStarted">
-          <div class="d-flex ai-center jc-sb text-bold text-40 white m-12">
+        <div v-if="gameStarted" :class="{ minimised }">
+          <div class="d-flex ai-center jc-sb text-bold white m-12" :class="minimised ? 'text-20' : 'text-40'">
             <img class="team-asset mr-12" :src="require('@/assets/img/coin.png')"> x {{ team.coins }}
           </div>
-          <div class="d-flex ai-center jc-sb text-bold text-40 white m-12">
+          <div class="d-flex ai-center jc-sb text-bold white m-12" :class="minimised ? 'text-20' : 'text-40'">
             <img class="team-asset mr-12" :src="require('@/assets/img/star.png')"> x {{ team.stars }}
           </div>
         </div>
@@ -44,6 +44,10 @@ export default {
     index: {
       type: Number,
       default: 0
+    },
+    minimised: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
@@ -59,5 +63,12 @@ export default {
 .team-asset {
   width: 60px;
   height: auto;
+}
+.minimised {
+  display: flex;
+  align-items: center;
+  .team-asset {
+    width: 30px;
+  }
 }
 </style>
