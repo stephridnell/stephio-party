1ce90b6d7f5a5ed60adb6aaccc7f76fafba2ab3d
Steph Ridnell <steph.ridnell@gmail.com>
2022-01-09 14:15:49 +1100

    Added turn component

---
+++ client/src/components/Turn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="turn">
+    Your turn
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Turn',
+  computed: {
+    ...mapGetters({
+      currentTurnDetails: 'currentTurnDetails',
+      currentTeam: 'currentTeam',
+      gameId: 'gameId'
+    })
+  },
+  mounted () {
+    if (!this.currentTurnDetails) {
+      this.$socket.emit('playerNewTurn', { gameId: this.gameId, teamId: this.currentTeam._id })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
