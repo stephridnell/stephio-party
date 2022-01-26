92657033bf87af91a3cae2e74819538d7785ed5b
Steph Ridnell <steph.ridnell@gmail.com>
2022-01-27 00:01:04 +1100

    Added respin button

--- client/src/components/MinigameRoulette.vue
+++ client/src/components/MinigameRoulette.vue
@@ -2,16 +2,19 @@
   <div class="minigame-roulette">
     <div class="event-overlay white p-60" v-if="randomMinigame">
       <img :src="s3BucketUrl + randomMinigame.imageName" :alt="randomMinigame.name">
-      <h2 class=" mt-24 text-bold text-5rem">{{ randomMinigame.name }}</h2>
-      <div class="text-3rem text-center" style="max-width:1200px">{{ randomMinigame.rules }}</div>
+      <h2 class=" mt-34 text-bold text-5rem">{{ randomMinigame.name }}</h2>
+      <div class="mt-34 text-3rem text-center" style="max-width:1200px">{{ randomMinigame.rules }}</div>
+      <button class="text-bold text-4rem mt-44" @click="$emit('done')">
+        Done
+      </button>
+      <button class="text-bold text-3rem mt-44 danger" @click="pickMinigame">
+        Re-spin
+      </button>
     </div>
     <div class="wrapper" ref="wrapper">
       <div id="minigame-roulette" class="window">
         <ul class="list">
           <li v-for="minigame in minigames" :key="minigame._id">
-            <img :src="s3BucketUrl + minigame.imageName" :alt="minigame.name">
-          </li>
-          <li v-for="minigame in minigames" :key="minigame._id + 2">
             <img :src="s3BucketUrl + minigame.imageName" :alt="minigame.name">
           </li>
         </ul>
@@ -48,16 +51,17 @@ export default {
   },
   methods: {
     pickMinigame () {
+      this.randomMinigame = null
       let minigameIndex = Math.floor(Math.random() * (this.max - this.min + 1) + this.min)
 
       this.$refs.wrapper.style.right = '0px'
       this.$refs.wrapper.animate({
-        right: '29570px'
-      }, 4000)
+        right: '14060px'
+      }, 5000)
       setTimeout(() => {
         this.randomMinigame = this.minigames[minigameIndex]
-        this.$refs.wrapper.style.right = '29570px'
-      }, 4000)
+        this.$refs.wrapper.style.right = '14060px'
+      }, 5000)
     }
   }
 }
